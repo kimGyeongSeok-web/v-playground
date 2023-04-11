55034335f3dc0e55a1d6ca1b05e2aba0786363b4
KKI147 <111959118+KKI147@users.noreply.github.com>
2023-04-11 11:58:53 +0900

    feat: office page input

--- component/UploadComponent.tsx
+++ component/UploadComponent.tsx
@@ -6,6 +6,8 @@ interface FormTypes {
   url: string;
   제목: string;
   태그: string;
+  설명: string;
+  기타: string;
 }
 
 interface CssProps {
@@ -16,12 +18,13 @@ export default function UploadComponent() {
   const validation = yup.object().shape({
     url: yup
       .string()
-      .required("url을 입력해주세요.")
+      .url("url을 입력해주세요.")
       .matches(
         /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
         "url 형식을 확인해주세요."
       ),
-    text: yup.string(),
+    제목: yup.string().required("제목을 입력해주세요"),
+    태그: yup.string().required("태그를 입력해주세요"),
   });
 
   const {
@@ -41,17 +44,48 @@ export default function UploadComponent() {
             <Button type="submit">업로드 하기</Button>
           </ButtonWrapper>
           <InPutDiv>
-            <InPutLabel htmlFor="url">파일 업로드하기</InPutLabel>
-            <InputUrl type="url" {...register("url")} />
+            <InPutLabel htmlFor="url">URL 주소 입력</InPutLabel>
+            <InputUrl
+              {...register("url")}
+              name="url"
+              placeholder="URL을 입력해주세요"
+            />
           </InPutDiv>
-
+          {errors.url && <Message>{errors.url.message}</Message>}
           <InPutDiv>
             <InPutLabel htmlFor="text">제목 입력</InPutLabel>
-            <InputText type="text" {...register("제목")} />
+            <InputText
+              {...register("제목")}
+              name="제목"
+              placeholder="제목을 입력해주세요"
+            />
+          </InPutDiv>
+          {/* 필수가 아니다 */}
+          <InPutDiv>
+            <InPutLabel htmlFor="text">설명 입력</InPutLabel>
+            <InputText
+              {...register("설명")}
+              name="설명"
+              placeholder="설명을 입력해주세요"
+            />
           </InPutDiv>
           <InPutDiv>
             <InPutLabel htmlFor="text">태그 입력</InPutLabel>
-            <InputText height={"200px"} type="text" {...register("태그")} />
+            <InputText
+              height={"200px"}
+              {...register("태그")}
+              name="태그"
+              placeholder="태그를 입력해주세요"
+            />
+          </InPutDiv>
+          {/* 필수가 아니다 */}
+          <InPutDiv>
+            <InPutLabel htmlFor="text">기타 입력</InPutLabel>
+            <InputText
+              {...register("기타")}
+              name="기타"
+              placeholder="기타를 입력해주세요"
+            />
           </InPutDiv>
         </UseForm>
       </WrapperLayout>
@@ -69,7 +103,11 @@ const WrapperLayout = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
-  padding: 100px 50px;
+  padding: 50px 50px;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const UseForm = styled.form`
@@ -105,7 +143,7 @@ const InPutLabel = styled.label`
   position: relative;
   display: flex;
   font-weight: 500;
-  font-size: 32px;
+  font-size: 24px;
   align-items: center;
   justify-content: center;
   border-radius: 12px;
@@ -114,22 +152,32 @@ const InPutLabel = styled.label`
   color: white;
   background-color: #4472c4;
   margin: 0 30px 0 0;
+  padding: 26px 0;
 `;
 
 const InputUrl = styled.input`
   position: relative;
-  width: 65%;
+  width: 75%;
   border: none;
   font-size: 32px;
   font-weight: 500;
+  padding: 5px 5px;
 `;
 
 const InputText = styled.input<CssProps>`
   position: relative;
-  width: 65%;
+  width: 75%;
   height: ${(props) => (props.height ? props.height : "auto")};
   font-size: 32px;
   border-radius: 12px;
   background-color: #d6dce5;
   border: none;
+  padding: 5px 10px;
+`;
+
+const Message = styled.div`
+  margin: 0 0 0 30%;
+  position: relative;
+  color: #ff0000;
+  font-size: 26px;
 `;
